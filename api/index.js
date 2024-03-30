4f494695d0a7fa7c385c4746866e61cd61fa90bd
Kongoloy <kongpopfaungfoo51@gmail.com>
2024-03-31 02:14:59 +0700

    connect to daata baseeee

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.connect(process.env.MONGOCONNECT)
+    .then(() => {
+        console.log('connected');
+    }).catch((err) => {
+        console.log(err)
+    })
 const app = express();
 
 app.listen(3000, () => {
