195b2b3fc9d5c8c7fd6e6e6a51c4b0c56c4e800f
Kongoloy <kongpopfuangfoo51@gmail.com>
2024-04-05 16:33:09 +0700

    yes

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react"
 import MenuCard from "./MenuCard"
+import "./Menu.css"
 export default function Menu() {
     const [cards, SetCards] = useState([{
+        cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        cardReviews: 2,
+        cardName: "coffee",
+        cardDes: "lorem",
+    }, {
+        cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        cardReviews: 2,
+        cardName: "coffee",
+        cardDes: "lorem",
+    }, {
+        cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        cardReviews: 2,
+        cardName: "coffee",
+        cardDes: "lorem",
+    }, {
         cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         cardReviews: 2,
         cardName: "coffee",
@@ -9,10 +25,10 @@ export default function Menu() {
     }])
 
     return (
-        <>
+        <div className="menu-container">
             {cards.map((card, index) => {
                 return <MenuCard key={index} cardImage={card.cardImage} cardReviews={card.cardReviews} cardDes={card.cardDes} cardName={card.cardName} />
             })}
-        </>
+        </div>
     )
 }
