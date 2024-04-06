2ad00ff3ab98b6d497016f60fc2ea1f8462af660
Kongoloy <kongpopfaungfoo51@gmail.com>
2024-04-07 00:49:11 +0700

    dont know

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -22,6 +22,21 @@ export default function Menu() {
         cardReviews: 2,
         cardName: "coffee",
         cardDes: "lorem",
+    }, {
+        cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        cardReviews: 2,
+        cardName: "coffee",
+        cardDes: "lorem",
+    }, {
+        cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        cardReviews: 2,
+        cardName: "coffee",
+        cardDes: "lorem",
+    }, {
+        cardImage: "https://images.unsplash.com/photo-1559496417-e7f25cb247f3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        cardReviews: 2,
+        cardName: "coffee",
+        cardDes: "lorem",
     }])
 
     return (
