04a954e7e67da50ec4fbe2471a8a1e981b8633bf
Kongoloy <kongpopfaungfoo51@gmail.com>
2024-05-15 09:14:21 +0700

    sameite

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -24,6 +24,7 @@ const authenticate = async (req, res, next) => {
             withCredentials: true,
             secure: true,
             httpOnly: true,
+            sameSite: 'none',
         });
         res.status(204).end();
 
